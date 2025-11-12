200a9be8ded3681548870136d42fd080c7bdc7da
Jash Jetly <jashjetly@gmail.com>
2025-11-13 01:40:10 +0530

    faltu design changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neutral: {
+          light: '#f9fafb',
+          DEFAULT: '#d1d5db',
+          dark: '#1f2937',
+        },
+        accent: {
+          light: '#fbbf24',
+          DEFAULT: '#f59e0b',
+          dark: '#b45309',
+        },
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
